5972fbfe7cc355000c02344dea9ffd90c5367cac
Brayden Aduddell <88568726+Hacksmith55555@users.noreply.github.com>
2025-05-08 22:17:30 -0500

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,33 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`0a000a0001010101010101010101020202020202020202020202020202020202020202020202020202020202020302020202020202020202020202020203020202020202020202020202040404040404040404040404040404040404040404040404040404040404`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.hazardWater,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.castle.tilePath5], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
